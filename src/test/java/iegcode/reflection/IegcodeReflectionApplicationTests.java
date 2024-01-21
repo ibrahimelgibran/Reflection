8f4f58ff982cf4a3bdc553f4e599f836bfc66510
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-01-22 00:12:46 +0700

    first commit

---
+++ src/test/java/iegcode/reflection/IegcodeReflectionApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.reflection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IegcodeReflectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
